7487328c953ed9720fa79dfb453c664e7524e25b
Michaël P.O <mike.pambo@gmail.com>
2019-06-03 01:10:42 +0200

    Update home picture

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="content-page">
-    <img class="flyer" src="~/assets/images/sido-mike-1.png" alt="Sido & Mike">
+    <img class="flyer" src="~/assets/images/sido-mike-3.jpg" alt="Sido & Mike">
   </section>
 </template>
 
 <style scoped>
 .flyer {
-  max-width: 400px;
+  width: 100%;
 }
 </style>
